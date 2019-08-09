2224a970b8552e950081d052186b548c583b07b4
alidev <aamansouri@gmail.com>
2019-08-09 19:42:51 -0400

    Added Q8.b to Ch2

--- Ch2/Ch2_Exercises.py
+++ Ch2/Ch2_Exercises.py
@@ -5,3 +5,7 @@
 import pandas as pd
 # Edit the path on your platform to correctly point to the file
 df = pd.read_csv('Datasets\College.csv')
+
+#### Part (b)
+df.head()
+df.rename(columns={df.columns.values[0]: 'Name'}, inplace=True)
