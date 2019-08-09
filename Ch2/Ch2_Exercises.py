ad74103d816694c1178bc325a4cd23d9d59c8e21
alidev <aamansouri@gmail.com>
2019-08-09 19:42:06 -0400

    Added Ch2 Exercises

---
+++ Ch2/Ch2_Exercises.py
@@ -0,0 +1,7 @@
+#%% [markdown]
+## Chapter 2 Exercises: Applied
+### Q8
+#### Part (a)
+import pandas as pd
+# Edit the path on your platform to correctly point to the file
+df = pd.read_csv('Datasets\College.csv')
